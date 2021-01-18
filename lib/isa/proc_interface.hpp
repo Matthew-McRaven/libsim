38f8afae876e817e99147ab83e782c03e38de128
Matthew McRaven <mkm302@georgetown.edu>
2021-01-18 13:55:53 +0000

    Purge C-style function pointers for C++ function objects.

--- lib/isa/proc_interface.hpp
+++ lib/isa/proc_interface.hpp
@@ -1,25 +1,28 @@
 #pragma once
 #include <stdint.h>
+#include <functional>
 
 namespace isa {
-template <typename addr_size_t, typename reg_size_t, typename CSR_size_t>
+template <typename addr_size_t, typename reg_size_t, typename csr_size_t>
 struct interface {
 	addr_size_t max_addr = {0xffff};
-	void (*begin_instruction)();
-	void (*end_instruction)();
+	
+	std::function<void()> begin_instruction;
+	std::function<void()> end_instruction;
 
-	void (*begin_transaction)();
-	void (*end_transaction)();
-	uint8_t  (*read_mem_byte)(addr_size_t address, bool supress_io);
-	void  (*write_mem_byte)(addr_size_t address, uint8_t value, bool supress_io);
-	void (*clear_mem)();
+	std::function<void()> begin_transaction;
+	std::function<void()> end_transaction;
+	std::function<uint8_t(addr_size_t, bool)> read_mem_byte;
+	std::function<void(addr_size_t, uint8_t, bool)> write_mem_byte;
+	std::function<void()> clear_mem;
 
-	reg_size_t (*read_reg)(uint8_t reg);
-	void (*write_reg)(uint8_t reg, reg_size_t value);
-	void (*clear_regbank)();
+	std::function<reg_size_t(uint8_t)> read_reg;
+	std::function<void(uint8_t, reg_size_t)> write_reg;
+	std::function<void()> clear_regs;
 
-	CSR_size_t (*read_csr)(uint8_t csr);
-	void (*write_csr)(uint8_t csr, CSR_size_t value);
-	void (*clear_CSR)();	
+	std::function<csr_size_t(uint8_t)> read_csr;
+	std::function<void(uint8_t, csr_size_t)> write_csr;
+	std::function<void()> clear_csr;
+	
 };
 };
