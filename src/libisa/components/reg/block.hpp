cab2155e6d434ab71234636d084c12134b3d9f28
Matthew McRaven <mkm302@georgetown.edu>
2021-01-27 18:39:47 +0000

    Port register bank to use storage devices.
    Passing unit tests indicates storage device has a sane API.

--- src/libisa/components/reg/block.hpp
+++ src/libisa/components/reg/block.hpp
@@ -3,29 +3,44 @@
 #include <stdint.h>
 #include <vector>
 
+#include "components/storage/block.hpp"
+
+// TODO: Migrate code to .TPP file.
 namespace components::reg {
 	template<typename reg_size_t, typename csr_size_t>
 	class block_register 
 	{
 	public:
 		block_register(uint8_t reg_count, uint8_t csr_count):
-			registers(reg_count), csrs(csr_count)
+			regs(reg_count), csrs(csr_count)
 		{
 
 		}
-		reg_size_t read_reg(uint8_t num) const {return registers[num];} 
-		void write_reg(uint8_t num, reg_size_t val) {registers[num]=val;}
+		reg_size_t read_reg(uint8_t num) const 
+		{
+			return regs.read(num);
+			} 
+		void write_reg(uint8_t num, reg_size_t val) 
+		{
+			regs.write(num, val);
+		}
 		void clear_regs(){
-			std::fill(registers.begin(), registers.end(), 0);
+			regs.clear();
 		}
 
-		csr_size_t read_csr(uint8_t num) const {return csrs[num];} 
-		void write_csr(uint8_t num, csr_size_t val) {csrs[num]=val;} 
+		csr_size_t read_csr(uint8_t num) const 
+		{
+			return csrs.read(num);
+		} 
+		void write_csr(uint8_t num, csr_size_t val) 
+		{
+			csrs.write(num, val);
+		} 
 		void clear_csrs(){
-			std::fill(csrs.begin(), csrs.end(), 0);
+			csrs.clear();
 		}
 	private:
-		std::vector<reg_size_t> registers;
-		std::vector<csr_size_t> csrs;
+		components::storage::storage_block<uint8_t, reg_size_t> regs;
+		components::storage::storage_block<uint8_t, csr_size_t> csrs;
 	};
 };
