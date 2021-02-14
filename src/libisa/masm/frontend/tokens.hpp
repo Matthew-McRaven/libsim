a27096a2795bcee847d8474d8caa822137ab6725
Matthew McRaven <mkm302@georgetown.edu>
2021-02-14 20:38:23 +0000

    Implement basic Pep/10 tokenizer.

--- src/libisa/masm/frontend/tokens.hpp
+++ src/libisa/masm/frontend/tokens.hpp
@@ -5,47 +5,51 @@
 #include <boost/spirit/include/phoenix_statement.hpp>
 #include <boost/spirit/include/phoenix_algorithm.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
-
+#include <boost/phoenix.hpp>
+#include <fmt/format.h>
 namespace masm::frontend
 {
 using namespace boost::spirit::qi;
 using namespace boost::spirit::ascii;
 namespace lex = boost::spirit::lex;
 
+static const std::string inner_lit = {R"( ([\w ]|\\(['"bfnrtv\\]|[xX][0-9a-f]{2})) )"};
 template <typename Lexer>
 struct tokenizer : lex::lexer<Lexer>
 {
 	tokenizer():
-	macro_ident("@[A-Za-z_]{1}(\\w*)"),
+	macro_ident("@[A-Za-z_]+"),
 	macro_arg("\\$[0-9]+"),
 	comma(","),
-	identifier("[A-Za-z|_]"),
+	identifier("[A-Za-z|_]+"),
 	sym_decl("[A-Za-z|_]:"),
-	dot_command("\\.[a-zA-Z]\\w*"),
-	//ch_lit("\'([^\'\\\\]|\\\\(\'|[bfnrtv]|\"|////)|\\\\[xX][0-9a-fA-F]{2})\'"),
-	//str_lit("((\")((([^\"\\\\])|((\\\\)([\'bfnrtv\"\\\\]))|((\\\\)(([x|X])([0-9A-Fa-f]{2}))))*)(\"))"),
-	empty(""),
+	dot_command("\\.[a-zA-Z]"),
+	ch_lit(fmt::format("'{}'", inner_lit)),
+	str_lit(fmt::format(R"(\"{}+\")", inner_lit)),
 	comment(";.*"),
 	hex_literal("0[xX][0-9a-fA-F]+"),
-	dec_literal("[+|-]{0,1}[0-9]+")
+	dec_literal("[01]?[0-9]+"),
+	empty("\\n"),
+	whitespaces("[( \\t)]")
 	{
-		this->self	
-			= macro_ident //[lex::_val = boost::phoenix::construct<std::string>(lex::_start + 1, lex::_end)]
-			| sym_decl //[lex::_val = boost::phoenix::construct<std::string>(lex::_start, lex::_end-1)]
-			| dot_command //[lex::_val = boost::phoenix::construct<std::string>(lex::_start + 1, lex::_end)]
-			| macro_arg
-			| comma
-			| hex_literal
-			| dec_literal
-			| ch_lit
-			| str_lit
-			| identifier
-			| empty
-			| comment
-			;
+		using boost::spirit::_1;
+		using boost::phoenix::construct;
+		this->self += whitespaces [lex::_pass = lex::pass_flags::pass_ignore]
+		| macro_ident [lex::_val = construct<std::string>(lex::_start + 1, lex::_end)]
+		| sym_decl[lex::_val = construct<std::string>(lex::_start, lex::_end-1)]
+		| dot_command
+		| macro_arg [lex::_val = construct<std::string>(lex::_start + 1, lex::_end)]
+		| comma
+		| hex_literal
+		| dec_literal
+		| str_lit [lex::_val = construct<std::string>(lex::_start + 1, lex::_end + 1)]
+		| ch_lit [lex::_val = construct<std::string>(lex::_start + 1, lex::_end + 1)]
+		| identifier
+		| empty [lex::_val = ""]
+		| comment [lex::_val = construct<std::string>(lex::_start + 1, lex::_end)];
 	}
 	lex::token_def<std::string> macro_ident, macro_arg, comma, identifier, sym_decl, dot_command, 
-		ch_lit, str_lit, empty, comment;
-	lex::token_def<long long> hex_literal, dec_literal;
+		ch_lit, str_lit, empty, comment, hex_literal, dec_literal;
+	lex::token_def<lex::omit> whitespaces;
 };
 }
