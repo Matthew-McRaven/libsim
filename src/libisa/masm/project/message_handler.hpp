7d2eb76448c41a49f9404cb43167ca81abce8944
Matthew McRaven <mkm302@georgetown.edu>
2021-02-18 18:18:18 +0000

    Implement error logging+lookup on message_handler.

--- src/libisa/masm/project/message_handler.hpp
+++ src/libisa/masm/project/message_handler.hpp
@@ -25,13 +25,9 @@ template <typename address_size_t>
 class message_handler
 {
 public:
-	void register_section(std::shared_ptr<masm::elf::code_section<address_size_t> >& parent, std::shared_ptr<masm::elf::code_section<address_size_t> >& child, uint32_t line);
-	void log_message(std::shared_ptr<masm::elf::code_section<address_size_t> >& section, uint32_t line, message message)
-	{
-
-	}
-	std::list<std::tuple<uint32_t /*line*/, message> > errors_for_section(std::shared_ptr<masm::elf::code_section<address_size_t> >& section);
-	void clear_errors();
+	void log_message(std::shared_ptr<masm::elf::code_section<address_size_t> > section, uint32_t line, message message);
+	std::list<std::tuple<uint32_t /*line*/, message> > errors_for_section(
+		std::shared_ptr<masm::elf::code_section<address_size_t> >& section, bool recurse=false);
 private:
 	using message_t = std::tuple<std::shared_ptr<masm::elf::code_section<address_size_t> > /*section*/, uint32_t /*line*/, message /*message*/>;
 	std::list<message_t> messages;
