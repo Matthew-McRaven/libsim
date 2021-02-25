5d54e440318945d88ba0a62de0ce8c1ca2bbbdcd
Matthew McRaven <mkm302@georgetown.edu>
2021-02-25 23:06:23 +0000

    Check if arguments fit in a given number of bytes.
    Add methods that check if the value stored in an argument can be represented with a number of byters.
    
    This is very helpful for the parser, which often needs to know if a value is 1 or 2 bytes.

--- test/libisa/masm/args.cpp
+++ test/libisa/masm/args.cpp
@@ -8,15 +8,24 @@ TEST_CASE( "Linear IR arguments, 16-bit, Character Arguments" ) {
 		auto cv = masm::ir::char_argument<uint16_t>("a");
 		CHECK(cv.argument_value() == (int)'a');
 		CHECK(cv.argument_string() == "a");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 	SECTION("Parse hex arguments") {
 		auto cv = masm::ir::char_argument<uint16_t>("\\xff");
 		CHECK(cv.argument_value() == 255);
 		CHECK(cv.argument_string() == "\\xff");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 
 		cv = masm::ir::char_argument<uint16_t>("\\xba");
 		CHECK(cv.argument_value() == 0xba);
 		CHECK(cv.argument_string() == "\\xba");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("Parse escape codes") {
@@ -32,6 +41,9 @@ TEST_CASE( "Linear IR arguments, 16-bit, Character Arguments" ) {
 			auto cv = masm::ir::char_argument<uint16_t>(k);
 			CHECK(cv.argument_value() == v);
 			CHECK(cv.argument_string() == k);
+			CHECK(!cv.fits_in(0));
+			CHECK(cv.fits_in(1));
+			CHECK(cv.fits_in(2));
 		}
 
 	}	
@@ -53,30 +65,48 @@ TEST_CASE( "Linear IR arguments, 16-bit, Decimal Arguments" ) {
 		auto cv = masm::ir::dec_argument<uint16_t>(255);
 		CHECK(cv.argument_value() == 255);
 		CHECK(cv.argument_string() == "255");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("Signed negative argument") {
 		auto cv = masm::ir::dec_argument<uint16_t>(0xffff);
 		CHECK(cv.argument_value() == 0xffff);
 		CHECK(cv.argument_string() == "-1");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 		cv = masm::ir::dec_argument<uint16_t>(0x8000);
 		CHECK(cv.argument_value() == 0x8000);
 		CHECK(cv.argument_string() == "-32768");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("Unsigned positive arguments") {
 		auto cv = masm::ir::unsigned_dec_argument<uint16_t>(255);
 		CHECK(cv.argument_value() == 255);
 		CHECK(cv.argument_string() == "255");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("Unsigned negative argument") {
 		auto cv = masm::ir::unsigned_dec_argument<uint16_t>(0xffff);
 		CHECK(cv.argument_value() == 0xffff);
 		CHECK(cv.argument_string() == "65535");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 		cv = masm::ir::unsigned_dec_argument<uint16_t>(0x8000);
 		CHECK(cv.argument_value() == 0x8000);
 		CHECK(cv.argument_string() == "32768");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 }
@@ -87,15 +117,24 @@ TEST_CASE( "Linear IR arguments, 16-bit, Hexadecimal Arguments" ) {
 		auto cv = masm::ir::hex_argument<uint16_t>(255);
 		CHECK(cv.argument_value() == 255);
 		CHECK(cv.argument_string() == "0x00FF");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("Unsigned negative argument") {
 		auto cv = masm::ir::hex_argument<uint16_t>(0xffff);
 		CHECK(cv.argument_value() == 0xffff);
 		CHECK(cv.argument_string() == "0xFFFF");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 		cv = masm::ir::hex_argument<uint16_t>(0x8000);
 		CHECK(cv.argument_value() == 0x8000);
 		CHECK(cv.argument_string() == "0x8000");
+		CHECK(!cv.fits_in(0));
+		CHECK(!cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 }
@@ -106,15 +145,24 @@ TEST_CASE( "Linear IR arguments, 16-bit, String Arguments" ) {
 		auto cv = masm::ir::string_argument<uint16_t>("a");
 		CHECK(cv.argument_value() == 'a');
 		CHECK(cv.argument_string() == "a");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 	SECTION("Parse hex arguments") {
 		auto cv = masm::ir::string_argument<uint16_t>("\\xff");
 		CHECK(cv.argument_value() == 255);
 		CHECK(cv.argument_string() == "\\xff");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 
 		cv = masm::ir::string_argument<uint16_t>("\\xba");
 		CHECK(cv.argument_value() == 0xba);
 		CHECK(cv.argument_string() == "\\xba");
+		CHECK(!cv.fits_in(0));
+		CHECK(cv.fits_in(1));
+		CHECK(cv.fits_in(2));
 	}
 
 	SECTION("escape codes") {
@@ -131,18 +179,30 @@ TEST_CASE( "Linear IR arguments, 16-bit, String Arguments" ) {
 			auto cv = masm::ir::string_argument<uint16_t>(k);
 			CHECK(cv.argument_value() == v);
 			CHECK(cv.argument_string() == k);
+			CHECK(!cv.fits_in(0));
+			CHECK(cv.fits_in(1));
+			CHECK(cv.fits_in(2));
 			// Check that characters can lead.
 			cv = masm::ir::string_argument<uint16_t>(k+"b");
 			CHECK(cv.argument_value() == (v<<8|'b'));
 			CHECK(cv.argument_string() == k+"b");
+			CHECK(!cv.fits_in(0));
+			CHECK(!cv.fits_in(1));
+			CHECK(cv.fits_in(2));
 			// Check that characters can trail.
 			cv = masm::ir::string_argument<uint16_t>("b"+k);
 			CHECK(cv.argument_value() == ('b'<<8|v));
 			CHECK(cv.argument_string() == "b"+k);
+			CHECK(!cv.fits_in(0));
+			CHECK(!cv.fits_in(1));
+			CHECK(cv.fits_in(2));
 			// Check that characters can be in both first and last.
 			cv = masm::ir::string_argument<uint16_t>(k+k);
 			CHECK(cv.argument_value() == (v<<8|v));
 			CHECK(cv.argument_string() == k+k);
+			CHECK(!cv.fits_in(0));
+			CHECK(!cv.fits_in(1));
+			CHECK(cv.fits_in(2));
 		}
 
 	}	
