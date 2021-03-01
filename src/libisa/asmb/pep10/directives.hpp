cf6021f78651c06527f798dd33e63f6c227f3688
Matthew McRaven <mkm302@georgetown.edu>
2021-03-01 12:36:48 -0800

    Add IR for .SYCALL.

--- src/libisa/asmb/pep10/directives.hpp
+++ src/libisa/asmb/pep10/directives.hpp
@@ -33,6 +33,31 @@ public:
 };
 
 template <typename address_size_t>
+class dot_sycall: public masm::ir::linear_line<address_size_t>
+{
+public:
+    dot_sycall();
+    ~dot_sycall() override = default;
+    dot_sycall(const dot_sycall& other);
+    dot_sycall& operator=(dot_sycall other);
+    std::shared_ptr<masm::ir::linear_line<address_size_t> > clone() const override;
+
+    // linear_line interface
+    std::string generate_listing_string() const override;
+    std::string generate_source_string() const override;
+
+    friend void swap(dot_sycall& first, dot_sycall& second)
+    {
+        using std::swap;
+        swap(static_cast<masm::ir::linear_line<address_size_t>&>(first), 
+            static_cast<masm::ir::linear_line<address_size_t>&>(second));
+        swap(first.argument, second.argument);
+    }
+
+    std::shared_ptr<masm::ir::symbol_ref_argument<address_size_t> > argument = nullptr;
+};
+
+template <typename address_size_t>
 class dot_usycall: public masm::ir::linear_line<address_size_t>
 {
 public:
@@ -57,93 +82,8 @@ public:
     std::shared_ptr<masm::ir::symbol_ref_argument<address_size_t> > argument = nullptr;
 };
 
-/*
-class DotExport: public AsmCode
-{
-    QSharedPointer<SymbolRefArgument> argument = nullptr;
-public:
-    DotExport() = default;
-    ~DotExport() override = default;
-    DotExport(const DotExport& other);
-    DotExport& operator=(DotExport other);
-    AsmCode *cloneAsmCode() const override;
-    void appendObjectCode(QList<int> &objectCode) const override;
 
-    // AsmCode interface
-    QString getAssemblerListing() const override;
-    QString getAssemblerSource() const override;
-    quint16 objectCodeLength() const override;
 
-    bool hasSymbolicOperand() const override;
-    QSharedPointer<const SymbolEntry> getSymbolicOperand() const override;
-    QSharedPointer<SymbolRefArgument> getArgument() const;
-    void setArgument(QSharedPointer<SymbolRefArgument>);
 
-    friend void swap(DotExport& first, DotExport& second)
-    {
-        using std::swap;
-        swap(static_cast<AsmCode&>(first), static_cast<AsmCode&>(second));
-        swap(first.argument, second.argument);
-    }
-};
-
-class DotSycall: public AsmCode
-{
-    QSharedPointer<SymbolRefArgument> argument = nullptr;
-public:
-    DotSycall() = default;
-    ~DotSycall() override = default;
-    DotSycall(const DotSycall& other);
-    DotSycall& operator=(DotSycall other);
-    AsmCode *cloneAsmCode() const override;
-    void appendObjectCode(QList<int> &objectCode) const override;
-
-    // AsmCode interface
-    QString getAssemblerListing() const override;
-    QString getAssemblerSource() const override;
-    quint16 objectCodeLength() const override;
-
-    bool hasSymbolicOperand() const override;
-    QSharedPointer<const SymbolEntry> getSymbolicOperand() const override;
-    QSharedPointer<SymbolRefArgument> getArgument() const;
-    void setArgument(QSharedPointer<SymbolRefArgument>);
-
-    friend void swap(DotSycall& first, DotSycall& second)
-    {
-        using std::swap;
-        swap(static_cast<AsmCode&>(first), static_cast<AsmCode&>(second));
-        swap(first.argument, second.argument);
-    }
-};
-
-class DotUSycall: public AsmCode
-{
-    QSharedPointer<SymbolRefArgument> argument = nullptr;
-public:
-    DotUSycall() = default;
-    ~DotUSycall() override = default;
-    DotUSycall(const DotUSycall& other);
-    DotUSycall& operator=(DotUSycall other);
-    AsmCode *cloneAsmCode() const override;
-    void appendObjectCode(QList<int> &objectCode) const override;
-
-    // AsmCode interface
-    virtual QString getAssemblerListing() const override;
-    QString getAssemblerSource() const override;
-    quint16 objectCodeLength() const override;
-
-    bool hasSymbolicOperand() const override;
-    QSharedPointer<const SymbolEntry> getSymbolicOperand() const override;
-    QSharedPointer<SymbolRefArgument> getArgument() const;
-    void setArgument(QSharedPointer<SymbolRefArgument>);
-
-    friend void swap(DotUSycall& first, DotUSycall& second)
-    {
-        using std::swap;
-        swap(static_cast<AsmCode&>(first), static_cast<AsmCode&>(second));
-        swap(first.argument, second.argument);
-    }
-};
-*/
 }; // End namespace asmb::pep10
 #include "directives.tpp"
--- src/libisa/asmb/pep10/directives.tpp
+++ src/libisa/asmb/pep10/directives.tpp
@@ -53,6 +53,61 @@ std::string asmb::pep10::dot_export<address_size_t>::generate_source_string() co
 	);
 
 }
+/*
+ * .SYCALL
+ */
+template <typename address_size_t>
+asmb::pep10::dot_sycall<address_size_t>::dot_sycall()
+{
+}
+
+template <typename address_size_t>
+asmb::pep10::dot_sycall<address_size_t>::dot_sycall(const asmb::pep10::dot_sycall<address_size_t>& other)
+{
+
+}
+
+template <typename address_size_t>
+asmb::pep10::dot_sycall<address_size_t> &asmb::pep10::dot_sycall<address_size_t>::operator=(
+	asmb::pep10::dot_sycall<address_size_t> other)
+{
+    swap(*this, other);
+    return *this;
+}
+
+
+template <typename address_size_t>
+std::shared_ptr<masm::ir::linear_line<address_size_t>> asmb::pep10::dot_sycall<address_size_t>::clone() const
+{
+    return std::make_shared<dot_sycall<address_size_t> >(*this);
+}
+
+template <typename address_size_t>
+std::string asmb::pep10::dot_sycall<address_size_t>::generate_listing_string() const
+{
+	auto temp = fmt::format("{:<6} {:<6}{}",
+		"",
+		"",
+		generate_source_string()
+	);
+	
+	return temp;
+}
+
+template <typename address_size_t>
+std::string asmb::pep10::dot_sycall<address_size_t>::generate_source_string() const
+{
+    auto dot_string = ".SYCALL";
+    auto operand_string = argument->string();
+	std::string comment = this->comment.value_or("");
+    return fmt::format("{:<9}{:<8}{:<12}{}",
+		"",
+		dot_string,
+		operand_string,
+		comment
+	);
+
+}
 
 /*
  * .USYCALL
