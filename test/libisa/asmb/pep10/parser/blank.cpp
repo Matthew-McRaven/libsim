c919fa7aa78330ad8aa0ae8bee03c0289bf4cd37
Matthew McRaven <mkm302@georgetown.edu>
2021-03-02 04:01:58 +0000

    Add unit tests for empty lines.

---
+++ test/libisa/asmb/pep10/parser/blank.cpp
@@ -0,0 +1,26 @@
+#include "catch.hpp"
+#include "helper.hpp"
+
+#include "asmb/pep10/ir.hpp"
+#include "masm/ir/directives.hpp"
+#include "masm/ir/empty.hpp"
+
+TEST_CASE( "Parse blank line", "[asmb::pep10::parser]"  ) {
+
+	auto driver = make_driver();
+
+	SECTION("Blank line") {	
+		auto project = masm::project::init_project<uint16_t>();
+		auto file = std::make_shared<masm::project::source_file>();
+		file->name = "main";
+		file->body = "\n\n";
+		std::vector<driver_t::source_t> vec = {file};
+		auto res = driver->assemble_project(project, vec, masm::project::toolchain_stage::SYMANTIC);
+		REQUIRE(res.first);
+		auto x = project->images[0]->sections[0];
+		REQUIRE(project->images[0]->sections[0]->body_ir->ir_lines.size() == 2);
+		auto maybe_empty = project->images[0]->sections[0]->body_ir->ir_lines[0];
+		auto as_empty = std::dynamic_pointer_cast<masm::ir::blank_line<uint16_t> >(maybe_empty);
+		REQUIRE(as_empty);
+	}
+}
