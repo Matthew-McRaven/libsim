ac2f2bbfed713dd6c122b6c891eab43e80bfb3b5
Matthew McRaven <mkm302@georgetown.edu>
2021-03-19 10:55:26 -0700

    (BAC-61) Add docs for symbol::entry.

--- src/libisa/symbol/entry.hpp
+++ src/libisa/symbol/entry.hpp
@@ -12,48 +12,55 @@ namespace symbol {
 template<typename value_t>
 class table;
 
+// Currently unused, will eventuallly be used to track trace tag information alongside a symbol.
+// TODO: Determine how to track debugging information.
 struct format
 {
     SymbolReprFormat format;
     uint32_t size = 0;
 };
-/*
- * A symbol can either be:
- *  1) Undefined: A symbol is not defined, and referenced 1+ times.
- *  2) Singlely Defined: A symbol is defined once, and referenced 0+ times.
- *  3) Multiply Defined: A symbol is defined 2+ times, and referenced 0+ times.
- */
 
-/*
- * A symbol entry represents one named symbol from a microprogram.
- * Symbols have multiple definition states that allow the microassembler to error if symbols are defined incorrectly.
- * Symbols' value can either be empty (i.e. undefined) or store the address of a line of microcode.
- * The symbol will automatically adjust between SINGLE and UNDEFINED based on the value pased to setValue(...),
- * but the class will not automatically set its definition state to MULTIPLE.
- * The function setMultiplyDefined() is the only way to notify a Symbol that it has been defined more than once.
- *
- * This function should be invoked from whatever assembler of compiler created the SymbolEntry - the SymbolTable will not do this for you.
- * This is because symbols could constantly be updated and changed (i.e. code relocation), so it would be very difficult for this class to decide if it has
- * been defined more than once, instead of simply being updated in place.
+
+/*!
+ * \brief A symbol represents a named value or location in memory that may be defined at most once.
+ * 
+ * Symbols provided by this namespace are meant to be flexibile--supporting microprogrammin and ELF usage.
+ * 
+ * Symbols that are undefined after linkage should cause an error; symbols defined multiple times should cause
+ * immediate errors.
+ * Some examples of symbol values are: currently undefined (symbol::empty), an address of a line of code
+ * (symbol::value_location), or a numeric constant (symbol::value_const)
+ * 
+ * As this class exposes all of its data publicly, it is up to the user to ensure that properties such as binding
+ * and definition state are updated correctly.
+ * This API decision was made because it is impossible for a symbol to know if its value is being set because it is defined,
+ * or if the value is being set to handle relocation.
+ * Simply put, the symbol doesn't know enough to update these fields.
+ * 
+ * \tparam value_t An unsigned integral type that is large enough to contain the largest address on the target system.
  */
 template<typename value_t>
 class entry
 {
 
 public:
-    //Default constructor, assumes value is SymbolEmpty
+    //Default constructor, assumes value is symbol::value_empty
     entry(typename symbol::table<value_t>& parent, std::string name);
     entry(typename symbol::table<value_t>& parent, std::string name, typename symbol::table<value_t>::value_ptr_t value);
     ~entry() = default;
-    type type() const {return value->type();}
 
-    // Non-owning reference to containing symbol table.
+    //! Non-owning reference to containing symbol table.
     typename symbol::table<value_t> const &  parent;
 
-    // Unique string name of symbol as appearing in the sources
+    //! Unique name as appearing in source code.
     std::string name;
+    //! Keep track of how many times this symbol's name has been defined.
     definition_state state;
+    /*! The binding type of this symbol (i.e., global vs local).
+     * \sa symbol::binding*/
     binding binding;
+    /*! The value taken on by this symbol.
+     * \sa symbol::abstract_value */
     typename symbol::table<value_t>::value_ptr_t value;
 };
 
