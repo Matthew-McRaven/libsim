a163f9ddd9ddc0f03d7771a3ff747ea0b6f91ac8
Matthew McRaven <mkm302@georgetown.edu>
2021-03-23 09:45:23 -0700

    WIP: Begin address assignment & elf packing.

---
+++ src/libisa/masm/elf/elfpack.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+/*
+ * At this point in the project, I needed some humor.
+ * The methods and classes described in this file are responsible for turning our IR into an ELF binary.
+ * That is, this code creates elves.
+ * 
+ * According to LoTR mythology, the supreme being Eru Ilúvatar creates elf-kind from his thoughts.
+ * Seeing as both this code and Eru are in the business of making elves, it seemed fitting to name the code after Eru.
+ * See: https://lotr.fandom.com/wiki/Eru_Il%C3%BAvatar
+ */
+
+namespace masm::backend
+{
+	class elf_pack
+	{
+
+	};
+
+	typedef elf_pack eru_illuvatar;
+}
