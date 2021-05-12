15d161ae5ebc6151e36a826bbf89f06b9348572e
Matthew McRaven <mkm302@georgetown.edu>
2021-05-12 20:00:28 +0000

    Add stubs for builtin tests.

---
+++ test/libisa/builtins/asmb/pep10/assemble_os.cpp
@@ -0,0 +1,32 @@
+#include "catch.hpp"
+#include "helper.hpp"
+
+#include "asmb/pep10/ir.hpp"
+#include "masm/ir/directives.hpp"
+#include "masm/ir/macro.hpp"
+
+TEST_CASE( "Parse entire OS", "[asmb::pep10::parser]"  ) {
+	using namespace builtins::pep10;
+	auto driver = make_driver();
+
+	SECTION("0-arity macro.") {	
+		auto project = masm::project::init_project<uint16_t>();
+		CHECK(project->macro_registry->register_macro("HELLO2", "@HELLO2 2\nASRA\nASRA\n", masm::MacroType::CoreMacro));
+		CHECK(project->macro_registry->register_macro("HELLO0", "@HELLO0 0\nASRA\nASRA\n", masm::MacroType::CoreMacro));
+		auto file = std::make_shared<masm::project::source_file>();
+		file->name = "main";
+		file->body = "@HELLO0\n.END\n";
+		std::vector<driver_t::source_t> vec = {file};
+		auto res = driver->assemble_project(project, vec, masm::project::toolchain_stage::SYMANTIC);
+
+		REQUIRE(res.first);
+		auto x = project->images[0]->sections[0];
+		REQUIRE(project->images[0]->sections[0]->body_ir->ir_lines.size() == 2);
+		auto maybe_macro = project->images[0]->sections[0]->body_ir->ir_lines[0];
+		auto as_macro = std::dynamic_pointer_cast<masm::ir::macro_invocation<uint16_t>>(maybe_macro);
+		REQUIRE(as_macro);
+		CHECK(as_macro->macro->body_ir->ir_lines.size() == 2);
+		CHECK(!as_macro->comment);
+	}
+
+}
---
+++ test/libisa/builtins/asmb/pep10/helper.cpp
@@ -0,0 +1,80 @@
+#include "./helper.hpp"
+
+std::shared_ptr<builtins::pep10::driver_t> builtins::pep10::make_driver()
+{
+	using namespace builtins::pep10;
+	auto driver = std::make_shared<driver_t>();
+
+	auto tokenizer = std::make_shared<masm::frontend::tokenizer<uint16_t, tokenizer_t> >();
+	transform_t tx_tokenizer = [=](project_t& proj, std::list<driver_t::work_t>& work) {
+		bool success = true;
+		driver_t::work_iterable_t result_work;
+		std::transform(work.begin(), work.end(), std::back_inserter(result_work),[&](auto& value){
+				success &= tokenizer->tokenize(proj, std::get<driver_t::section_t>(value));
+				return driver_t::work_iterable_t::value_type{stage_t::TOKEN, value};
+			});
+		return driver_t::result_t{success, result_work};
+	};
+	driver->register_transform(tx_tokenizer, stage_t::RAW);
+
+	auto preproc = std::make_shared<masm::frontend::preprocessor<uint16_t, tokenizer_t> >();
+	transform_t tx_preproc = [=](project_t& proj, std::list<driver_t::work_t>& work) {
+		bool success = true;
+		driver_t::work_iterable_t result_work;
+		for(auto value : work) {
+			auto [local_success, local_children] = preproc->preprocess(proj, std::get<driver_t::section_t>(value));
+			for(auto child : local_children) {
+				result_work.emplace_back(driver_t::work_iterable_t::value_type{stage_t::RAW, 
+					std::static_pointer_cast<masm::elf::code_section<uint16_t>>(child)});
+			}
+			result_work.emplace_back(driver_t::work_iterable_t::value_type{stage_t::PREPROCESS, value});
+			success &= local_success;
+		}
+		return driver_t::result_t{success, result_work};
+	};
+	driver->register_transform(tx_preproc, stage_t::TOKEN);
+
+	auto parser = std::make_shared<asmb::pep10::parser>();
+	transform_t tx_parser = [=](project_t& proj, std::list<driver_t::work_t>& work) {
+		bool success = true;
+		driver_t::work_iterable_t result_work;
+		std::transform(work.begin(), work.end(), std::back_inserter(result_work),[&](auto& value){
+				success &= parser->parse(proj, std::get<driver_t::section_t>(value));
+				return driver_t::work_iterable_t::value_type{stage_t::SYMANTIC, value};
+			});
+		return driver_t::result_t{success, result_work};
+	};
+	driver->register_transform(tx_parser, stage_t::PREPROCESS);
+
+	masm::backend::region<uint16_t> node;
+	node.input_sections = {".text"};
+	node.base_address = 0;
+	transform_t tx_addr = [=](project_t& proj, std::list<driver_t::work_t>& work) {
+		bool success = true;
+		std::set<std::shared_ptr<masm::elf::image<uint16_t>>> images;
+		for(auto& value : work) {
+			images.insert(std::get<driver_t::section_t>(value)->containing_image.lock());
+		}
+
+		driver_t::work_iterable_t result_work;
+		std::transform(images.begin(), images.end(), std::back_inserter(result_work),[&](auto image){
+			masm::backend::assign_image(proj, image, {node});
+			return driver_t::work_iterable_t::value_type{stage_t::ADDRESS_ASSIGN, image};
+		});
+		return driver_t::result_t{success, result_work};
+	};
+	driver->register_transform(tx_addr, stage_t::SYMANTIC);
+
+	transform_t tx_elf = [=](project_t& proj, std::list<driver_t::work_t>& work) {
+		bool success = true;
+		driver_t::work_iterable_t result_work;
+		std::transform(work.begin(), work.end(), std::back_inserter(result_work),[&](auto value){
+			auto [local_success, stream] = masm::elf::pack_image(proj, std::get<driver_t::image_t>(value));
+			success &= local_success;
+			return driver_t::work_iterable_t::value_type{stage_t::PACK, value};
+		});
+		return driver_t::result_t{success, result_work};
+	};
+	driver->register_transform(tx_elf, stage_t::ADDRESS_ASSIGN);
+	return driver;
+}
