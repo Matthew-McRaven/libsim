7d94c4f4d90d4b0a3265928547cc1f7c8005688e
Matthew McRaven <mkm302@georgetown.edu>
2021-05-28 12:40:02 -0700

    Fix OS unit test name.

--- test/libisa/builtins/asmb/pep10/assemble_os.cpp
+++ test/libisa/builtins/asmb/pep10/assemble_os.cpp
@@ -10,7 +10,7 @@ TEST_CASE( "Parse entire OS", "[asmb::pep10::parser]"  ) {
 	using namespace asmb::pep10::driver;
 	auto driver = make_driver();
 
-	SECTION("0-arity macro.") {	
+	SECTION("Assemble OS.") {	
 		auto project = masm::project::init_project<uint16_t>();
 		auto ex = registry::instance();
 		for(const auto& macro : ex.macros()){
