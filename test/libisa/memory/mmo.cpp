c5dcadc382d9523276e334affed585a4a9eb448f
Matthew McRaven <mkm302@georgetown.edu>
2021-07-20 15:52:13 -0700

    (BAC-164) Test "unwrite" functionality for output.

--- test/libisa/memory/mmo.cpp
+++ test/libisa/memory/mmo.cpp
@@ -22,4 +22,24 @@ TEST_CASE( "Validate memory-mapped output storage") {
 		CHECK(*_2 == 20);
 	}
 
+		SECTION("Allocate / write / unwrite")
+	{
+		// Create memory-mapped input storage.
+		auto in = components::storage::Output<uint16_t, true>(0);
+		auto endpoint = in.endpoint();
+		REQUIRE(in.write(0, 10).has_value());
+		REQUIRE(in.write(0, 20).has_value());
+		auto _1 = endpoint->next_value();
+		REQUIRE(_1);
+		CHECK(*_1 == 10);
+		auto _2 = endpoint->next_value();
+		REQUIRE(_2);
+		CHECK(*_2 == 20);
+		// Undo any writes.
+		in.take_delta().value()->apply_backward();
+		auto _3 = endpoint->next_value();
+		REQUIRE(_3);
+		CHECK(*_3 == 0);
+	}
+
 }
