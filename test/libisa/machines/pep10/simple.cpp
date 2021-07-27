5dec2de3ce361c3ef0c94c8c83c0430b6040047c
Matthew McRaven <mkm302@georgetown.edu>
2021-07-27 14:52:49 -0700

    (BAC-159) Add checks for NZVC bits.

--- test/libisa/machines/pep10/simple.cpp
+++ test/libisa/machines/pep10/simple.cpp
@@ -20,4 +20,15 @@ TEST_CASE("Simple static tests of Pep/10 computer", "[isa::pep10]")
 		CHECK(machine->read_register(isa::pep10::Register::A) == 0xDEAD);
 		CHECK(machine->read_register(isa::pep10::Register::TR) == 0xBEEF);
 	}
+	SECTION("Test R/W NZVC")
+	{
+		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0x1'0000);
+		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
+		CHECK(machine->read_csr(isa::pep10::CSR::N) == false);
+		CHECK(machine->read_csr(isa::pep10::CSR::C) == false);
+		machine->write_csr(isa::pep10::CSR::N, true);
+		machine->write_csr(isa::pep10::CSR::C, true);
+		CHECK(machine->read_csr(isa::pep10::CSR::N) == true);
+		CHECK(machine->read_csr(isa::pep10::CSR::C) == true);
+	}
 }
