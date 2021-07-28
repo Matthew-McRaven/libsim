ebc4b9386b1211545d5ddc771eccd6ca601d492b
Matthew McRaven <mkm302@georgetown.edu>
2021-07-27 21:04:46 -0700

    Add LWDX,i; LDWA,d tests.
    Knowing these instructions work is necessary for creating simple programs that "test out" more complex addressing modes.
    LDWX,i is necessary because X is used for complex addressing modes.
    LDWA,d is necessary because it allows building programs with global variables.

--- test/libisa/machines/pep10/simple_traces.cpp
+++ test/libisa/machines/pep10/simple_traces.cpp
@@ -39,4 +39,34 @@ TEST_CASE("Simple execution trace tests of Pep/10 computer", "[isa::pep10]")
 		CHECK(machine->read_register(isa::pep10::Register::PC) == 0x003);
 	}
 
+	 // Test that loads can effect the index register.
+	SECTION("Test index load immediate.")
+	{
+		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
+		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
+		// Object code for `LDWX 0x1234,i`
+		auto x = std::vector<uint8_t>{0x48, 0x12, 0x34};
+		isa::pep10::load_bytes(machine, x, 0).value();
+		auto ret = machine->step();
+		REQUIRE(ret.has_value());
+		CHECK(ret.value());
+		CHECK(machine->read_register(isa::pep10::Register::X) == 0x1234);
+		CHECK(machine->read_register(isa::pep10::Register::PC) == 0x003);
+	}
+
+	// Test addressing mode d.
+	SECTION("Test accumulator load direct.")
+	{
+		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
+		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
+		// Object code for `LDWA 0x0000,d`
+		auto x = std::vector<uint8_t>{0x41, 0x00, 0x00};
+		isa::pep10::load_bytes(machine, x, 0).value();
+		auto ret = machine->step();
+		REQUIRE(ret.has_value());
+		CHECK(ret.value());
+		CHECK(machine->read_register(isa::pep10::Register::A) == 0x4100);
+		CHECK(machine->read_register(isa::pep10::Register::PC) == 0x003);
+	}
+
 }
