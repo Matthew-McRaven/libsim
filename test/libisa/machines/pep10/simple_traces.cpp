89924e7e2db9f0e567e5c499e37f888229753707
Matthew McRaven <mkm302@georgetown.edu>
2021-07-27 20:55:17 -0700

    Add unit tests for load_bytes, LDWA.
    Must validate these functions work in order to validate remaining instructions.

--- test/libisa/machines/pep10/simple_traces.cpp
+++ test/libisa/machines/pep10/simple_traces.cpp
@@ -21,5 +21,22 @@ TEST_CASE("Simple execution trace tests of Pep/10 computer", "[isa::pep10]")
 		CHECK(ret.value());
 		CHECK(machine->read_register(isa::pep10::Register::SP) == 0x0006);
 	}
+	// Tests ability to bring data into A register.
+	// Tests ability to execute nonunary instructions.
+	// Tests load_bytes.
+	// Tests correct endianess of operand decoding.
+	SECTION("Test accumulator immediate load.")
+	{
+		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
+		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
+		// Object code for `LDWA 0x1234,i`
+		auto x = std::vector<uint8_t>{0x40, 0x12, 0x34};
+		isa::pep10::load_bytes(machine, x, 0).value();
+		auto ret = machine->step();
+		REQUIRE(ret.has_value());
+		CHECK(ret.value());
+		CHECK(machine->read_register(isa::pep10::Register::A) == 0x1234);
+		CHECK(machine->read_register(isa::pep10::Register::PC) == 0x003);
+	}
 
 }
