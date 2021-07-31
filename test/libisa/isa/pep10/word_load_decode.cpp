136fd5c54a77333695e48495afe838b194bf851e
Matthew McRaven <mkm302@georgetown.edu>
2021-07-31 01:01:11 +0000

    (BAC-182) Improve comments on load-word-style tests.

--- test/libisa/isa/pep10/word_load_decode.cpp
+++ test/libisa/isa/pep10/word_load_decode.cpp
@@ -11,6 +11,14 @@
 // Don't do any program simulation here.
 TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 {
+	/*
+	 * Each test case checks the following:
+	 *	Word loads are litte endian. That is, the first byte is the high-order, and the second byte is the low-order.
+	 * 	Operands are decoded according to the correct addressing mode.
+	 * Tests do not need to ensure that each instruction's addressing modes are ordered correctly--other tests ensure this.
+	 * To achieve this with a minimal number of instructions, tests are allowed to mutate registers directly.
+	 * This reduces the number of instructions that must be validated before running these tests to include only LDWA.
+	 */
 	// Test addressing mode i
 	SECTION("Test accumulator immediate load.")
 	{
@@ -30,7 +38,8 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,d`
+		// Object code for `LDWA 0x0003,d\n .WORD 0xDEAD`
+		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x41, 0x00, 0x03, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
 		auto ret = machine->step();
@@ -44,8 +53,8 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0003,n\n.WORD 0x0000`
-		// This should load `0x0003` into the A register
+		// Object code for `LDWA 0x0003,n\n.WORD 0x05\n.WORD 0XDEAD`
+		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x42, 0x00, 0x03, 0x00, 0x05, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
 		auto ret = machine->step();
@@ -59,7 +68,7 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,n\n.WORD 0xDEAD`
+		// Object code for `LDWA 0x0001,s\n.WORD 0xDEAD`
 		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x43, 0x00, 0x01, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
@@ -75,7 +84,7 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,n\n.WORD 0xDEAD`
+		// Object code for `LDWA 0x0002,sf\n.WORD 0x05\n.WORD 0xDEAD`
 		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x44, 0x00, 0x02, 0x00, 0x05, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
@@ -91,7 +100,7 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,n\n.WORD 0xDEAD`
+		// Object code for `LDWA 0x0000,x\n.WORD 0xDEAD`
 		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x45, 0x00, 0x01, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
@@ -107,7 +116,7 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,n\n.WORD 0xDEAD`
+		// Object code for `LDWA 0x0001,sx\n.WORD 0xDEAD`
 		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x46, 0x00, 0x01, 0xDE, 0xAD};
 		isa::pep10::load_bytes(machine, x, 0).value();
@@ -124,7 +133,7 @@ TEST_CASE("Test operand decoding for loading words", "[isa::pep10]")
 	{
 		auto storage = std::make_shared<components::storage::Block<uint16_t, true, uint8_t>>(0xFFFF);
 		auto machine = std::make_shared<isa::pep10::LocalMachine<true>>(storage);
-		// Object code for `LDWA 0x0000,n\n.WORD 0xDEAD`
+		// Object code for `LDWA 0x0000,sfx\n.WORD 0xDEAD\n.WORD 0x02`
 		// This should load `0xDEAD` into the A register
 		auto x = std::vector<uint8_t>{0x47, 0x00, 0x01, 0xDE, 0xAD, 0x00, 0x02};
 		isa::pep10::load_bytes(machine, x, 0).value();
