1bb14487ef980c643f993e527bc9c23f230030e3
Matthew McRaven <mkm302@georgetown.edu>
2021-08-08 21:51:27 +0000

    Enable code coverage in debug builds.

--- build-native-dbg.sh
+++ build-native-dbg.sh
@@ -6,6 +6,6 @@ cd build
 	source /emsdk/emsdk_env.sh
 	mkdir -p native
 	cd native
-	cmake ../.. -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug
+	cmake ../.. -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug -Dcode_coverage=1
 	make -j$(nproc) && make test -j$(nproc)
 )
