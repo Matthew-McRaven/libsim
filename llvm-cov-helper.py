c92021e7012c202b9743a9977dcb858bd85e1bb9
Matthew McRaven <mkm302@georgetown.edu>
2021-08-08 21:52:12 +0000

    Add helper script to handle llvm-cov invocation.

---
+++ llvm-cov-helper.py
@@ -0,0 +1,22 @@
+import os
+import stat
+
+from pathlib import Path
+
+profraws = [str(profraw.absolute()) for profraw in Path('.').rglob('*.profraw')]
+print(profraws)
+
+os.system(f'llvm-profdata merge {" ".join(profraws)} --output coverage.profdata')
+
+executable = stat.S_IEXEC | stat.S_IXUSR
+tests = []
+for root, dirs, files in os.walk('build/coverage'):
+	if "CMake" in root: continue
+	for filename in files:
+		filename = root+"/"+filename
+		if os.path.isfile(filename):
+			st = os.stat(filename)
+			if st.st_mode & executable: tests.append(filename)
+regex = "\"(catch)|(elfio/*)|(ngraph)|(magic_enum)|(fmt)|(outcome)|(test/*)\""
+os.system(f'llvm-cov export --ignore-filename-regex={regex} --instr-profile coverage.profdata --format=lcov {" --object ".join(tests)}> coverage.lcov')
+os.system("genhtml -o \"coverage\" coverage.lcov")
