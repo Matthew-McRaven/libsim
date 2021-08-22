1aba1cda9b32b6646a584568108c5d833b738b4b
Matthew McRaven <mkm302@georgetown.edu>
2021-08-22 17:50:25 +0000

    (BAC-213) Implement unit tests for OS ROM creation.
    Every time we change the OS, these tests will need to be updated.
    However, this will ensure that tweaking the assembler didn't accidentally break the OS.

--- test/libisa/elf_tools/pep10/pep10.cpp
+++ test/libisa/elf_tools/pep10/pep10.cpp
@@ -3,6 +3,7 @@
 
 #include "asmb/pep10/ir.hpp"
 #include "asmb/pep10/create_driver.hpp"
+#include "elf_tools/pep10/mem_map.hpp"
 #include "ex_registry.hpp"
 #include "masm/ir/directives.hpp"
 #include "masm/ir/macro.hpp"
@@ -31,8 +32,19 @@ std::shared_ptr<ELFIO::elfio> os_to_image()
 TEST_CASE( "Convert ELF image to Pep/10 machine", "[elf_tools::pep10]"  ) {
 
 
-	SECTION("Assemble OS.") {	
-		auto image = os_to_image();
+	SECTION("Assemble OS successfully.") {	
+		REQUIRE(os_to_image() != nullptr);
 	}
 
+	SECTION("ROM in correct location and is correct size.") {	
+		auto image = os_to_image();
+		auto ROM_result = elf_tools::pep10::construct_os_storage<false>(*image);
+		REQUIRE(ROM_result.has_value());
+		auto [ROM_offset, ROM_storage] = ROM_result.value();
+		// OS ROM should not unexpectedly change in size.
+		CHECK(ROM_offset == 0xFAAF);
+		CHECK(ROM_storage->max_offset() == 0x550);
+		// And the default OS should go right up to the maximum offset.
+		CHECK(ROM_offset + ROM_storage->max_offset() == 0xFFFF);
+	}
 }
