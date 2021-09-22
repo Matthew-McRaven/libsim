63bb91247b9df0eceecf42b2c782f23577a8fa44
Matthew McRaven <mkm302@georgetown.edu>
2021-09-22 02:40:18 +0000

    (BAC-233) Add address<=>listing line mapping classes.
    These classes are designed for ease of serialization to an ELF container.
    I could probably get away with only a start address vs start+end, but this is an easy fix in the future.

---
+++ src/libisa/masm/elf/addr_line_mapping.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdint>
+
+#include <cereal/types/vector.hpp>
+
+namespace masm::elf
+{
+	// Struct to represent an address span to listing line bijection.
+	// It is designed to be serialized easily using ceral.
+	// Take care that you pass the right address type, or parsing will probably implode.
+	template <typename addr_size_t>
+	struct AddressLineRegion
+	{
+		addr_size_t start_address, end_address;
+		std::size_t line_number;
+		bool contains(addr_size_t addr) const {
+			return start_address <= addr && addr <= end_address;
+		}
+	private:
+		friend class cereal::access;
+		template <class Archive>
+		void serialize( Archive & ar )
+		{
+			ar(start_address, end_address, line_number);
+		}
+	};
+
+	// Struct to represent a collection of address<=>line mappings, since I can't serialize a vector directly.
+	// It is designed to be serialized easily using ceral.
+	// Take care that you pass the right address type, or parsing will probably implode.
+	template <typename addr_size_t>
+	struct AddressLineRegionVector
+	{
+		std::vector<AddressLineRegion<addr_size_t>> vec;
+		/*
+		 * The API really should return an iterator rather than bool+optional<size_t>.
+		 * However, in the one place that it is used, this pattern would lead to ugly code.
+		 * So, this ugly API design will stay for now.
+		 */
+		bool contains(addr_size_t addr) const {
+			for(const auto& item : vec) 
+				if(item.contains(addr)) return true;
+			return false;
+		}
+
+		// Must return optional, since there is no gaurentee addresses will be contained within vector, and I don't want
+		// contains(x) == true to be a precondition of this function working correctly. 
+		std::optional<std::size_t> line(addr_size_t addr) const {
+			for(const auto& item : vec) 
+				if(item.contains(addr)) return item.line_number;
+			return std::nullopt;
+		}
+	private:
+		friend class cereal::access;
+		template <class Archive>
+		void serialize( Archive & ar )
+		{
+			ar(vec);
+		}
+	};
+}
